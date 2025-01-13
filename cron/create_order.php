fa2d436dcc6799c7ae1c667c96099f895cb214dc
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-01-13 02:59:07 +0100

    refactor: cron job to optimize db query and handle errors

--- cron/create_order.php
+++ cron/create_order.php
@@ -6,33 +6,51 @@ use WooWMS\Utils\Logger;
 
 require_once __DIR__ . '/../../../../wp-load.php';
 
+define('WOO_WMS_DOING_CRON', true);
+
 $logger = Logger::init();
 
-$order_statuses = [ 'completed' ];
+echo 'Cron has started creating orders' . PHP_EOL;
 
-$orders = [];
-
-foreach ( $order_statuses as $order_status ) {
+try {
 	$args = [
-		'status' => $order_status,
-		'limit'  => - 1
+		'status'     => 'completed',
+		'meta_query' => [
+			[
+				'key'     => Logicas::$META_WMS_LOGICAS_ORDER_ID,
+				'compare' => 'NOT EXISTS'
+			]
+		],
+		'date_query' => [
+			[
+				'column' => 'post_date_gmt',
+				'after'  => '1 day ago'
+			]
+		],
+		'limit'      => - 1
 	];
 	
-	$orders = array_merge( $orders, wc_get_orders( $args ) );
+	$query = new WC_Order_Query( $args );
+	
+	$orders = $query->get_orders();
+	
+	foreach ( $orders as $order ) {
+		$wms_logicas_order_id = $order->get_meta( Logicas::$META_WMS_LOGICAS_ORDER_ID );
+		
+		if ( ! empty( $wms_logicas_order_id ) ) {
+			continue;
+		}
+		
+		$order_id = (int) $order->get_id();
+		echo 'Current order ID: ' . $order_id . PHP_EOL;
+		
+		// Log the process for debugging.
+		$logger->info( 'Cron triggered woo_wms_cron_create_order for order ID: ' . $order_id . PHP_EOL );
+		
+		do_action( 'woo_wms_cron_create_order', $order_id );
+	}
+} catch ( Exception $e ) {
+	$logger->error( $e->getMessage() );
 }
 
-foreach ( $orders as $order ) {
-	$wms_logicas_order_id = $order->get_meta( Logicas::$META_WMS_LOGICAS_ORDER_ID );
-	
-	if ( $wms_logicas_order_id ) {
-		continue;
-	}
-	
-	$order_id = (int) $order->get_id();
-	echo $order_id;
-	
-	do_action( 'woo_wms_cron_create_order', $order_id );
-	
-	// Log the process for debugging.
-	$logger->info( 'Cron triggered woo_wms_cron_create_order for order ID: ' . $order_id );
-}
+echo 'Cron has finished creating orders' . PHP_EOL;
