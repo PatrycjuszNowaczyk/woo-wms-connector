47e91b83e74ca54b256737532b787bc66c31c5a8
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-02-20 15:03:23 +0100

    fix: error message if user is not authorized to connect with WMS api

--- WooWMS/Services/Logicas.php
+++ WooWMS/Services/Logicas.php
@@ -86,14 +86,18 @@ class Logicas {
 			if ( is_wp_error( $response )) {
 				throw new Exception( $response->get_error_message() );
 			} else if ( 400 <= $response_code) {
-				$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
-				$message = $response_body['message'] ?? $response_body;
+				if ( 401 === $response_code ) {
+					$message = __('Your request has not been properly authorized.');
+				} else {
+					$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
+					$message = $response_body['message'] ?? (string) $response_body ?: __( 'There is no message from WMS api.' );
+				}
 				throw new Exception( $message );
 			}
 			
 			return json_decode( wp_remote_retrieve_body( $response ) );
 		} catch ( Exception $e ) {
-			$error_message = __('Error from WMS API:', 'woo_wms_connector') . PHP_EOL . $e->getMessage();
+			$error_message = __('Error from WMS API:', 'woo_wms_connector') . ' ' . $e->getMessage();
 			throw new Exception( $error_message );
 		}
 	}
