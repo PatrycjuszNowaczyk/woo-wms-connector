d553c3b454543bc9597de7a5e18b1d7820093c9a
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-02-25 00:57:03 +0100

    chore: format code

--- WooWMS/Services/Logicas.php
+++ WooWMS/Services/Logicas.php
@@ -64,7 +64,7 @@ class Logicas {
 	private function request( string $url, string $method = 'GET', array $data = [] ) {
 		try {
 			$method = strtoupper( $method );
-			$args = [
+			$args   = [
 				'method'  => $method,
 				'headers' => [
 					'X-Auth-Token' => $this->get_right_token( $url )
@@ -79,12 +79,12 @@ class Logicas {
 				$args['body'] = json_encode( $data );
 			}
 			
-			$response = wp_remote_request( $url, $args );
+			$response      = wp_remote_request( $url, $args );
 			$response_code = wp_remote_retrieve_response_code( $response );
 			
-			if ( is_wp_error( $response )) {
+			if ( is_wp_error( $response ) ) {
 				throw new Exception( $response->get_error_message() );
-			} else if ( 400 <= $response_code) {
+			} else if ( 400 <= $response_code ) {
 				if ( 401 === $response_code ) {
 					$message = __( 'Your request has not been properly authorized.', 'woo_wms_connector' );
 				} else {
@@ -101,11 +101,11 @@ class Logicas {
 		}
 	}
 	
-	private function get_shipping_method(object $order): object|null {
-		$shipping_items = $order->get_items('shipping');
-		$shipping_item = null;
+	private function get_shipping_method( object $order ): object|null {
+		$shipping_items = $order->get_items( 'shipping' );
+		$shipping_item  = null;
 		
-		foreach ($shipping_items as $item) {
+		foreach ( $shipping_items as $item ) {
 			$shipping_item = $item;
 			break;
 		}
@@ -121,7 +121,7 @@ class Logicas {
 	 * @return void
 	 */
 	public function create_order( int $orderId ): void {
-		if ( ! function_exists( 'WooWMS\Services\create_products_order_array' ) ) {
+		if ( !function_exists( 'WooWMS\Services\create_products_order_array' ) ) {
 			function create_products_order_array( array $items_to_send, string $sku, int $item_quantity ): array {
 				$key = array_search( $sku, array_column( $items_to_send, 'sku' ) );
 				if ( $key !== false ) {
@@ -150,16 +150,16 @@ class Logicas {
 			}
 			
 			// declare variables
-			$order_items   = $order->get_items();
-			$shipping_method = $this->get_shipping_method($order);
+			$order_items     = $order->get_items();
+			$shipping_method = $this->get_shipping_method( $order );
 			
 			if ( empty( $shipping_method ) ) {
 				throw new Exception( 'Shipping method not found in order with ID: ' . $orderId );
 			}
 			
 			$shipping_method_id = $shipping_method->get_method_id();
-			$parcel_machine_id = $order->get_meta( self::$META_PARCEL_MACHINE_ID ) ?: $_POST[ self::$META_PARCEL_MACHINE_ID ] ?? null;
-			$items_to_send = [];
+			$parcel_machine_id  = $order->get_meta( self::$META_PARCEL_MACHINE_ID ) ?: $_POST[ self::$META_PARCEL_MACHINE_ID ] ?? null;
+			$items_to_send      = [];
 			
 			// add products to items_to_send
 			foreach ( $order_items as $item ) {
@@ -170,12 +170,12 @@ class Logicas {
 				if ( str_contains( $product_sku, '|' ) ) {
 					$product_sku = explode( '|', $product_sku );
 				} else {
-					$product_sku = trim($product_sku);
+					$product_sku = trim( $product_sku );
 				}
 				
 				if ( is_array( $product_sku ) ) {
 					foreach ( $product_sku as $sku ) {
-						$sku = trim($sku);
+						$sku           = trim( $sku );
 						$items_to_send = create_products_order_array( $items_to_send, $sku, $item_quantity );
 					}
 				} else {
@@ -192,22 +192,22 @@ class Logicas {
 				'warehouse_code'   => $this->warehouseCode,
 				'shipping_method'  => 'innoship.' . strtolower( $shipping_method_id ),
 				'shipping_address' => [
-					'zip'        => trim($order->get_shipping_postcode()),
-					'city'       => trim($order->get_shipping_city()),
-					'email'      => trim($order->get_billing_email()),
-					'line1'      => trim($order->get_shipping_address_1()),
-					'line2'      => trim($order->get_shipping_address_2()),
-					'phone'      => trim($order->get_shipping_phone()),
-					'last_name'  => trim($order->get_shipping_last_name()),
-					'first_name' => trim($order->get_shipping_first_name()),
-					'country'    => trim($order->get_shipping_country())
+					'zip'        => trim( $order->get_shipping_postcode() ),
+					'city'       => trim( $order->get_shipping_city() ),
+					'email'      => trim( $order->get_billing_email() ),
+					'line1'      => trim( $order->get_shipping_address_1() ),
+					'line2'      => trim( $order->get_shipping_address_2() ),
+					'phone'      => trim( $order->get_shipping_phone() ),
+					'last_name'  => trim( $order->get_shipping_last_name() ),
+					'first_name' => trim( $order->get_shipping_first_name() ),
+					'country'    => trim( $order->get_shipping_country() )
 				],
-				'shipping_comment' => trim($order->get_customer_note()),
+				'shipping_comment' => trim( $order->get_customer_note() ),
 				'order_number'     => $order->get_order_number(),
 				'items'            => $items_to_send
 			];
 			
-			if ( 'inpost' === $shipping_method_id && ! empty($parcel_machine_id) ) {
+			if ( 'inpost' === $shipping_method_id && !empty( $parcel_machine_id ) ) {
 				$orderData['shipping_address']['box_name'] = $parcel_machine_id;
 			}
 			
@@ -222,7 +222,7 @@ class Logicas {
 			$this->logger->info( 'Order data send to WMS: ' . json_encode( $orderData ) );
 			$this->logger->info( 'Order data in WMS: ' . json_encode( $orderResponse ) );
 			
-			$shipResponse = $this->request($this->apiBaseUrl . '/store/v2/orders/' . $orderResponse->id . '/ship', 'POST');
+			$shipResponse = $this->request( $this->apiBaseUrl . '/store/v2/orders/' . $orderResponse->id . '/ship', 'POST' );
 			
 			$this->logger->info( 'Ship data in WMS: ' . json_encode( $shipResponse ) );
 			
@@ -232,9 +232,9 @@ class Logicas {
 				&& isset( $_GET['action'] )
 				&& 'woo_wms_create_order' === $_GET['action']
 			) {
-				wp_send_json_success([
-					'message' => __('Order created', 'woo_wms_connector')
-				], 200);
+				wp_send_json_success( [
+					'message' => __( 'Order created', 'woo_wms_connector' )
+				], 200 );
 			}
 			
 			$this->update_shop_stocks();
@@ -246,9 +246,9 @@ class Logicas {
 				defined( 'DOING_AJAX' ) && DOING_AJAX
 				&& isset( $_GET['action'] ) && 'woo_wms_create_order' === $_GET['action']
 			) {
-				wp_send_json_error([
+				wp_send_json_error( [
 					'message' => $e->getMessage()
-				], 500);
+				], 500 );
 			}
 			
 			if ( defined( 'WOO_WMS_DOING_CRON' ) && WOO_WMS_DOING_CRON ) {
@@ -269,12 +269,12 @@ class Logicas {
 		try {
 			if (
 				false === is_admin() ||
-				(isset($_GET['page']) && 'wc-orders' !== $_GET['page'] && isset($_GET['action']) && 'edit' !== $_GET['action'])
+				( isset( $_GET['page'] ) && 'wc-orders' !== $_GET['page'] && isset( $_GET['action'] ) && 'edit' !== $_GET['action'] )
 			) {
 				return;
 			}
 			
-			if (0 === count($order->get_changes())) {
+			if ( 0 === count( $order->get_changes() ) ) {
 				return;
 			}
 			
@@ -310,47 +310,45 @@ class Logicas {
 			}
 			$stocks = $stocks->items;
 			
-			$all_products = wc_get_products([
+			$all_products = wc_get_products( [
 				'status' => 'publish',
 				'limit'  => -1
-			]);
+			] );
 			
-			foreach ($all_products as $product) {
-				if ($product->get_children()) {
-					foreach ($product->get_children() as $child) {
+			foreach ( $all_products as $product ) {
+				if ( $product->get_children() ) {
+					foreach ( $product->get_children() as $child ) {
 						$all_products[] = wc_get_product( $child );
 					}
 				}
 			}
 			
-			foreach ($all_products as $product) {
-				if ( str_contains($product->get_sku(), '|') ) {
-					$skus = array_fill_keys(explode('|', $product->get_sku()), 0);
-					$skus = array_map('trim', $skus);
+			foreach ( $all_products as $product ) {
+				if ( str_contains( $product->get_sku(), '|' ) ) {
+					$skus = array_fill_keys( explode( '|', $product->get_sku() ), 0 );
+					$skus = array_map( 'trim', $skus );
 					
 					foreach ( $skus as $sku => $qty ) {
-						$skus[$sku] = $stocks[
-							array_search($sku, array_column($stocks, 'sku') )
-						]->quantity;
+						$skus[ $sku ] = $stocks[ array_search( $sku, array_column( $stocks, 'sku' ) ) ]->quantity;
 					}
 					
-					$minimal_quantity = min($skus);
-					$product->set_stock_quantity((int) $minimal_quantity);
+					$minimal_quantity = min( $skus );
+					$product->set_stock_quantity( (int) $minimal_quantity );
 					$product->save();
 					
 					continue;
 				}
 				
-				$stock_index = array_search($product->get_sku(), array_column($stocks, 'sku') );
+				$stock_index = array_search( $product->get_sku(), array_column( $stocks, 'sku' ) );
 				
-				if ( $product->get_sku()) {
+				if ( $product->get_sku() ) {
 					$product->set_stock_quantity(
 						false !== $stock_index ? $stocks[ $stock_index ]->quantity : 0
 					);
 					$product->save();
 				}
 			}
-
+			
 			
 			if (
 				defined( 'DOING_AJAX' )
@@ -358,9 +356,9 @@ class Logicas {
 				&& isset( $_GET['action'] )
 				&& 'woo_wms_update_shop_stocks' === $_GET['action']
 			) {
-				wp_send_json_success([
-					'message' => __('Stocks updated successfully!', 'woo_wms_connector')
-				], 200);
+				wp_send_json_success( [
+					'message' => __( 'Stocks updated successfully!', 'woo_wms_connector' )
+				], 200 );
 			}
 			
 		} catch ( Exception $e ) {
@@ -372,9 +370,9 @@ class Logicas {
 				&& isset( $_GET['action'] )
 				&& 'woo_wms_update_shop_stocks' === $_GET['action']
 			) {
-				wp_send_json_error([
+				wp_send_json_error( [
 					'message' => $e->getMessage()
-				], 500);
+				], 500 );
 			}
 		}
 	}
@@ -389,11 +387,11 @@ class Logicas {
 	public function cancel_order( object $order ): void {
 		try {
 			$is_wms_order_cancelled = $order->get_meta( self::$META_WMS_LOGICAS_IS_ORDER_CANCELLED );
-			if ( ! empty($is_wms_order_cancelled) ) {
+			if ( !empty( $is_wms_order_cancelled ) ) {
 				return;
 			}
 			
-			$order_id = $order->get_id();
+			$order_id     = $order->get_id();
 			$wms_order_id = $order->get_meta( self::$META_WMS_LOGICAS_ORDER_ID );
 			
 			$orderResponse = $this->request( $this->apiBaseUrl . '/store/v2/orders/' . $wms_order_id . '/cancel', 'POST' );
@@ -407,20 +405,20 @@ class Logicas {
 			
 			$this->logger->info( 'Canceled shop order id: ' . $order_id . ' | ' . 'Canceled wms order id: ' . $wms_order_id );
 			
-			add_action('admin_notices', function () use ($order_id) {
+			add_action( 'admin_notices', function () use ( $order_id ) {
 				echo '<div class="notice notice-success is-dismissible">
-					<p>' . sprintf(__('Order nr %s was successfully canceled in WMS', 'woo_wms_connector'), $order_id) . '</p>
+					<p>' . sprintf( __( 'Order nr %s was successfully canceled in WMS', 'woo_wms_connector' ), $order_id ) . '</p>
 				</div>';
-			}, 0, 0);
+			}, 0, 0 );
 			
 			
 		} catch ( Exception $e ) {
 			
-			add_action('admin_notices', function () use ($order_id) {
+			add_action( 'admin_notices', function () use ( $order_id ) {
 				echo '<div class="notice notice-warning is-dismissible">
-					<p>' . sprintf(__('Order nr %s was not canceled in Logicas warehouse', 'woo_wms_connector'), $order_id) . '</p>
+					<p>' . sprintf( __( 'Order nr %s was not canceled in Logicas warehouse', 'woo_wms_connector' ), $order_id ) . '</p>
 				</div>';
-			}, 0, 0);
+			}, 0, 0 );
 			
 			$this->logger->error( $e->getMessage() );
 		} finally {
@@ -458,9 +456,9 @@ class Logicas {
 				$orders_statuses[ $order_id ] = $wms_order->status;
 			}
 			
-			$orders_statuses = array_map(function ($status) {
-				return ucfirst(str_replace('-', ' ', $status));
-			}, $orders_statuses);
+			$orders_statuses = array_map( function ( $status ) {
+				return ucfirst( str_replace( '-', ' ', $status ) );
+			}, $orders_statuses );
 			
 			wp_send_json_success( [
 				'orders_statuses' => $orders_statuses
@@ -483,10 +481,10 @@ class Logicas {
 	public function create_product( array $product ): void {
 		$required_fields = [
 			'manufacturer' => '( ' . __( 'Manufacturer', 'woo_wms_connector' ) . ' )',
-			'sku'=> '( ' . __( 'SKU', 'woo_wms_connector' ) . ')',
-			'ean' => '( ' . __( 'GTIN, UPC, EAN, or ISBN', 'woo_wms_connector' ) . ' )',
-			'name'=> '( ' . __( 'WMS name', 'woo_wms_connector' ) . ' )',
-			'weight' => '( ' . __( 'Weight (kg)', 'woo_wms_connector' ) . ' )'
+			'sku'          => '( ' . __( 'SKU', 'woo_wms_connector' ) . ')',
+			'ean'          => '( ' . __( 'GTIN, UPC, EAN, or ISBN', 'woo_wms_connector' ) . ' )',
+			'name'         => '( ' . __( 'WMS name', 'woo_wms_connector' ) . ' )',
+			'weight'       => '( ' . __( 'Weight (kg)', 'woo_wms_connector' ) . ' )'
 		];
 		
 		$missing_fields = [];
@@ -498,27 +496,28 @@ class Logicas {
 				}
 			}
 			
-			if ( ! empty( $missing_fields ) ) {
+			if ( !empty( $missing_fields ) ) {
 				$message = sprintf(
-					__("Product with SKU <code>%s</code> has missing required fields: %s.", 'woo_wms_connector'),
+					__( "Product with SKU <code>%s</code> has missing required fields: %s.", 'woo_wms_connector' ),
 					$product['sku'],
-					join(', ', $missing_fields)
+					join( ', ', $missing_fields )
 				);
 				Utils::set_admin_notice( $message, AdminNoticeType::WARNING );
+				
 				return;
 			}
-		
+			
 			$response = $this->request( $this->apiBaseUrl . '/management/v2/products', 'POST', $product );
 			
-			if ( false === empty( $response->id) ) {
+			if ( false === empty( $response->id ) ) {
 				update_post_meta( $product['id'], 'wms_id', $response->id );
 			}
 			
-			$message = sprintf( __('Product with SKU <code>%s</code> has been created.', 'woo_wms_connector'), $product['sku'] );
+			$message = sprintf( __( 'Product with SKU <code>%s</code> has been created.', 'woo_wms_connector' ), $product['sku'] );
 			Utils::set_admin_notice( $message, AdminNoticeType::SUCCESS );
 		} catch ( Exception $e ) {
 			if ( str_contains( $e->getMessage(), $product['sku'] ) || str_contains( $e->getMessage(), $product['ean'] ) ) {
-				if ( $this->sync_wc_product_data_with_wms( $product, $this->get_list_of_products()) ) {
+				if ( $this->sync_wc_product_data_with_wms( $product, $this->get_list_of_products() ) ) {
 					$message = sprintf( __( "Product with SKU number <code>%s</code> already exists in the WMS database. All data stored in WMS was synchronized to match product data stored in WooCommerce with WMS data.", 'woo_wms_connector' ), $product['sku'] );
 					Utils::set_admin_notice( $message, AdminNoticeType::INFO );
 				}
@@ -549,7 +548,7 @@ class Logicas {
 				sprintf( __( 'Product with SKU <code>%s</code> has been updated.', 'woo_wms_connector' ), $product['sku'] )
 				: __( 'Product has been updated.', 'woo_wms_connector' );
 			Utils::set_admin_notice( $message, AdminNoticeType::SUCCESS );
-		
+			
 		} catch ( Exception $e ) {
 			$this->logger->error( $e->getMessage() );
 			Utils::set_admin_notice( $e->getMessage(), AdminNoticeType::ERROR );
@@ -572,7 +571,7 @@ class Logicas {
 				throw new Exception( __( "WMS's product ID is required", 'woo_wms_connector' ), 400 );
 			}
 			
-			$product = wc_get_product(  $product_id );
+			$product = wc_get_product( $product_id );
 			
 			$response = $this->request( $this->apiBaseUrl . '/management/v2/product/' . $product_wms_id, 'DELETE' );
 			
@@ -592,14 +591,14 @@ class Logicas {
 	 */
 	public function get_list_of_products(): array {
 		try {
-			$products = [];
-			$page = 1;
-			$response = $this->request( $this->apiBaseUrl . '/management/v2/products?page=' . $page, 'GET' );
+			$products    = [];
+			$page        = 1;
+			$response    = $this->request( $this->apiBaseUrl . '/management/v2/products?page=' . $page, 'GET' );
 			$total_pages = $response->meta->pagination->totalPages;
-			$products = $response->items;
+			$products    = $response->items;
 			
-			for( $page = 2; $page <= $total_pages; $page++ ) {
-				$response = $this->request( $this->apiBaseUrl . '/management/v2/products?page=' . $page, 'GET' );
+			for ( $page = 2; $page <= $total_pages; $page++ ) {
+				$response   = $this->request( $this->apiBaseUrl . '/management/v2/products?page=' . $page, 'GET' );
 				$products[] = $response->items;
 			}
 		} catch ( Exception $e ) {
@@ -631,7 +630,7 @@ class Logicas {
 					break;
 				}
 			}
-
+			
 			
 			if ( empty( $wms_product ) ) {
 				$message = <<<TEXT
@@ -676,7 +675,7 @@ class Logicas {
 			Utils::set_admin_notice( $message, AdminNoticeType::ERROR );
 		} finally {
 			if ( true === $product_data_fetched ) {
-				set_transient('wms_sync_product_data_' . $product_data['id'], true, 300);
+				set_transient( 'wms_sync_product_data_' . $product_data['id'], true, 300 );
 			}
 		}
 		
@@ -695,8 +694,8 @@ class Logicas {
 			}
 			
 			$manufacturers = null;
-			$page = 1;
-			$total_pages = 1;
+			$page          = 1;
+			$total_pages   = 1;
 			
 			$response = $this->request( $this->apiBaseUrl . "/management/v2/manufacturers?page=$page" );
 			
@@ -704,12 +703,12 @@ class Logicas {
 				throw new Exception( __( 'Manufacturers not found', 'woo_wms_connector' ), 404 );
 			}
 			
-			$total_pages = $response->meta->pagination->totalPages;
+			$total_pages   = $response->meta->pagination->totalPages;
 			$manufacturers = $response->items;
 			
 			if ( $total_pages > $page ) {
 				for ( $page = 2; $page <= $total_pages; $page++ ) {
-					$response = $this->request( $this->apiBaseUrl . "/management/v2/manufacturers?page=$page" );
+					$response      = $this->request( $this->apiBaseUrl . "/management/v2/manufacturers?page=$page" );
 					$manufacturers = array_merge( $manufacturers, $response->items );
 				}
 			}
